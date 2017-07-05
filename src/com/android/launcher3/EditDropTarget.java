AOSPA/android_packages_apps_Launcher3

Launcher3: support changing icons and app label
This introduces a new tab called customization.
It can be accessed by longpressing the homescreen and pressing the paint icon.
It supports selecting a custom icon pack.
Once a custom icon pack has been selected you can drag any icon you want to the edit drop target.
Once done, it will launch a dialog allowing you to change the app name.
When pressing on the app icon inside the dialog, an activity will launch giving you the option to select a new icon for your current app.

Change-Id: I4a55dfb2ca42be20d2e81e7f8ec2a8ae207ee165
Signed-off-by: Anas Karbila <anaskarbila@gmail.com>

src/com/android/launcher3/EditDropTarget.java
@@ -1,0 +1,73 @@
+/*
+ * Copyright (C) 2017 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.launcher3;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.util.AttributeSet;
+
+public class EditDropTarget extends UninstallDropTarget {
+
+    private Launcher mLauncher;
+
+    public EditDropTarget(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public EditDropTarget(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        mLauncher = Launcher.getLauncher(context);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        // Get the hover color
+        mHoverColor = Utilities.getColorAccent(getContext());
+
+        setDrawable(R.drawable.ic_edit_launcher);
+    }
+
+    @Override
+    protected boolean supportsDrop(DragSource source, ItemInfo info) {
+        return info instanceof AppInfo || info instanceof ShortcutInfo;
+    }
+
+    @Override
+    public void completeDrop(DragObject d) {
+        Bitmap bitmap = null;
+        ComponentName componentName = null;
+        ItemInfo info = d.dragInfo;
+        DropTargetResultCallback callback = d.dragSource instanceof DropTargetResultCallback
+                ? (DropTargetResultCallback) d.dragSource : null;
+        if (info instanceof AppInfo) {
+            componentName = ((AppInfo) info).componentName;
+            bitmap = ((AppInfo) info).iconBitmap;
+        } else if (info instanceof ShortcutInfo) {
+            componentName = ((ShortcutInfo) info).intent.getComponent();
+            bitmap = ((ShortcutInfo) info).getIcon(LauncherAppState.getInstance().getIconCache());
+        }
+
+        if (bitmap != null && componentName != null) {
+            mLauncher.startEdit(bitmap, info, componentName);
+        }
+        if (componentName != null && callback != null) {
+            sendUninstallResult(mLauncher, true, componentName, info.user, callback);
+        }
+    }
+}

BenzoRoms/packages_apps_Launcher3

Signed-off-by: mydongistiny <jaysonedson@gmail.com>

src/com/android/launcher3/EditDropTarget.java
@@ -1,0 +1,68 @@
+/*
+ * Copyright (C) 2017 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.launcher3;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.util.AttributeSet;
+
+public class EditDropTarget extends ButtonDropTarget {
+
+    private Launcher mLauncher;
+
+    public EditDropTarget(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public EditDropTarget(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        mLauncher = Launcher.getLauncher(context);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        // Get the hover color
+        mHoverColor = Utilities.getColorAccent(getContext());
+
+        setDrawable(R.drawable.ic_edit_launcher);
+    }
+
+    @Override
+    protected boolean supportsDrop(DragSource source, ItemInfo info) {
+        return info instanceof AppInfo || info instanceof ShortcutInfo;
+    }
+
+    @Override
+    public void completeDrop(DragObject d) {
+        Bitmap bitmap = null;
+        ComponentName componentName = null;
+        ItemInfo info = d.dragInfo;
+        if (info instanceof AppInfo) {
+            componentName = ((AppInfo) info).componentName;
+            bitmap = ((AppInfo) info).iconBitmap;
+        } else if (info instanceof ShortcutInfo) {
+            componentName = ((ShortcutInfo) info).intent.getComponent();
+            bitmap = ((ShortcutInfo) info).getIcon(LauncherAppState.getInstance().getIconCache());
+        }
+
+        if (bitmap != null && componentName != null) {
+            mLauncher.startEdit(bitmap, info, componentName);
+        }
+    }
+}